b88ceca35164a088a1e8b9dce635f9ec2b71f655
Carlos Trujillo <augusto21_20@hotmail.com>
2023-05-27 19:43:13 -0500

    Cambios

---
+++ src/photos-module/data/array-socials.ts
@@ -0,0 +1,28 @@
+import { Social } from "../interfaces/socials/socials.interface";
+
+export const socials: Social[] = [
+	{
+		name: 'Facebook',
+		source: '../../assets/shared/desktop/facebook.svg'
+	},
+
+	{
+		name: 'YouTube',
+		source: '../../assets/shared/desktop/youtube.svg'
+	},
+
+	{
+		name: 'Twitter',
+		source: '../../assets/shared/desktop/twitter.svg'
+	},
+	
+	{
+		name: 'Pinterest',
+		source: '../../assets/shared/desktop/pinterest.svg'
+	},
+
+	{
+		name: 'Instagram',
+		source: '../../assets/shared/desktop/instagram.svg'
+	}
+];
