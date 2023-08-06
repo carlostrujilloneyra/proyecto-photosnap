683bd4affc01a9f6ed2cbb95daad241f9a700033
Carlos Trujillo <augusto21_20@hotmail.com>
2023-08-06 01:11:40 -0500

    Corrigiendo el despliegue

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './SCSS/styles.scss';
 import { App } from './App'
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
